0ec55e4c49a5ff4d3a4f9a913e6320509009d5b4
BernardoVieira1 <bernardovieira50@gmail.com>
2022-10-05 23:22:00 -0300

    first commit

---
+++ src/components/Form/style.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  formContext: {
+    width: "100%",
+    height: "100%",
+    bottom: 0,
+    backgroundColor: "#ffff",
+    alignItems: "center",
+    marginTop: 30,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  form:{
+    width: "100%",
+    height: "auto",
+    marginTop: 30,
+    padding: 10,
+  },
+  formLabel: {
+    fontSize: 18,
+    color: "#000",
+    paddingLeft: 20,
+
+  },
+  formInput: {
+    width: "90%",
+    borderRadius: 10,
+    height: 60,
+    fontSize: 16,
+    backgroundColor: "#f6f6f6",
+    margin: 12,
+    paddingLeft: 10,
+  },
+  buttonCalculator:{
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+    backgroundColor: "#ff0043",
+    paddingTop: 14,
+    paddingBottom: 14,
+    marginLeft: 12,
+    margin: 10,
+  },
+  textButtonCalculator:{
+    fontSize: 20,
+    color: "#ffff",
+  }
+
+});
+
+export default styles;
